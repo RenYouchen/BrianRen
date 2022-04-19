c37e045ff3712e8bad6a335aacdbaf3667404ef1
renyouchen <renyouchen3@gmail.com>
2022-04-19 10:43:08 +0800

    First Commit

---
+++ src/chiscoj/com/b005.java
@@ -0,0 +1,14 @@
+package chiscoj.com;
+
+import java.util.Scanner;
+
+public class b005 {
+    public static void main(String[] args) {
+        String a;
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            a = sc.nextLine();
+            System.out.printf("hello, %s\n", a);
+        }
+    }
+}
