d8fd919263b94d1023bda4e7b32e6e6bf588bc16
renyouchen <renyouchen3@gmail.com>
2022-04-19 19:34:36 +0800

    b005 finsh

--- src/chiscoj/com/b005.java
+++ src/chiscoj/com/b005.java
@@ -6,7 +6,7 @@ public class b005 {
     public static void main(String[] args) {
         String a;
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
+        while (sc.hasNextLine()) {
             a = sc.nextLine();
             System.out.printf("hello, %s\n", a);
         }
