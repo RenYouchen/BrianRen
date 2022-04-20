7faf8bf1ae59f0736ec250a516bac492c4dbd1c5
renyouchen <renyouchen3@gmail.com>
2022-04-20 13:21:31 +0800

    b008 finish add sort test

---
+++ src/chiscoj/com/b009.java
@@ -0,0 +1,22 @@
+package chiscoj.com;
+
+import java.util.Scanner;
+
+public class b009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String hr,min,fNum;
+        int num;
+        while (sc.hasNextInt()) {
+            hr = sc.next();
+            min = sc.next();
+            fNum = hr + min;
+            num = Integer.parseInt(fNum);
+            if (num >= 1700 || num < 730) {
+                System.out.println("Off School");
+            } else {
+                System.out.println("At School");
+            }
+        }
+    }
+}
