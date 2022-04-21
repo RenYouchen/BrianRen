c8b15fae8374efacffcd987d447176636f62b4f8
renyouchen <renyouchen3@gmail.com>
2022-04-21 20:44:05 +0800

    b012 finish!

--- src/chiscoj/com/b012.java
+++ src/chiscoj/com/b012.java
@@ -1,4 +1,19 @@
 package chiscoj.com;
 
+import java.util.Scanner;
+
 public class b012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num , digits;
+        while (sc.hasNextInt()){
+            digits = 1;
+            num = sc.nextInt();
+            while (num >= 10){
+                num = num / 10;
+                digits++;
+            }
+            System.out.println(digits);
+        }
+    }
 }
