790d5720864c92f207d1d9a764d4dd43237f6a3f
BrianRen <46395700+RenYouchen@users.noreply.github.com>
2022-09-30 10:22:19 +0000

    uwu

---
+++ CPP/資訊培訓/day1/2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n = 10;
+	float f = 10.5;
+	bool b = 1;
+	char c = 'A';
+	cout << n <<" "<< f << " " << b << " " << c << '\n';
+}
+
+
+
