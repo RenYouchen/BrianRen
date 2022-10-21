01436080a02ffba7e744a3ba9a02fe482e25e0fe
BrianRen <46395700+RenYouchen@users.noreply.github.com>
2022-10-21 09:11:15 +0000

    c290 finish

--- CPP/zerojudge/c290.cpp
+++ CPP/zerojudge/c290.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 string input;
+int A = 0, B = 0;
 
-int main(){
-    cin >> input ;
-    cout << input;
+int ans() {
+    cin >> input;
+    for (int i = 0; i < input.size(); i += 2) {
+        A += input[i] - '0';
+        if (i + 1 < input.size())
+            B += input[i + 1] - '0';
+    }
+
+    if (A > B) {
+        return A - B;
+    } else {
+        return B - A;
+    }
+}
+
+int main() {
+    cout << ans() << '\n';
 }
