fe4bd2975f7ce641b526ce6fa2388f440b67a984
BrianRen <46395700+RenYouchen@users.noreply.github.com>
2022-10-22 16:32:54 +0000

    eof

---
+++ CPP/EOF.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    while (1) {
+        cin >> a;
+        if (a == EOF) {
+            cout << "EOF" << '\n';
+            break;
+        } else {
+            cout << a << '\n';
+        }
+    }
+}
---
+++ CPP/zerojudge/i399.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+using namespace std;
+int ans(){
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    //for (int i = 0; i < n; i++) {
+    //    ans();
+    //}
+    ans();
+}
