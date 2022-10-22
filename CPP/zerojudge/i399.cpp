6ba8d960d1168e90a80c5b60b72afbf65e98d755
BrianRen <46395700+RenYouchen@users.noreply.github.com>
2022-10-22 17:15:25 +0000

    i399 AC uwu

--- CPP/zerojudge/i399.cpp
+++ CPP/zerojudge/i399.cpp
@@ -2,12 +2,24 @@
 using namespace std;
 int ans() {
     int a[3];
+    int P = 1;
     for (int i = 0; i < 3; i++) {
         cin >> a[i];
     }
-    sort(a, a + 3);
+
+    sort(a, a + 3, greater<int>());
+
     for (int i = 0; i < 3; i++) {
-        cout << a[i];
+        if (a[i] == a[i + 1 % 3]) {
+            a[i] = 10;
+            P++;
+            }
+    }
+    cout << P << ' ';
+    for (int i = 0; i < 3; i++) {
+        if (a[i] != 10) {
+            cout << a[i] << ' ';
+        }
     }
 }
 
