8f9611612b9f04db914b570ab456cc43e6197769
BrianRen <46395700+RenYouchen@users.noreply.github.com>
2022-10-22 16:41:51 +0000

    a

--- CPP/zerojudge/i399.cpp
+++ CPP/zerojudge/i399.cpp
@@ -1,17 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 #include <bits/stdc++.h>
 using namespace std;
-int ans(){
-    
+int ans() {
+    int a[3];
+    for (int i = 0; i < 3; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + 3);
+    for (int i = 0; i < 3; i++) {
+        cout << a[i];
+    }
 }
 
-int main(){
-    int n;
-    cin >> n;
-    //for (int i = 0; i < n; i++) {
-    //    ans();
-    //}
+int main() {
+    // int n;
+    // cin >> n;
+    // for (int i = 0; i < n; i++) {
+    //     ans();
+    // }
     ans();
 }
