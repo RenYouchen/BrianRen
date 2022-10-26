2b4964080c163ef53d2ed30db6c52ff9e30e6712
BrianRen <46395700+RenYouchen@users.noreply.github.com>
2022-10-26 17:42:21 +0000

    vid

--- python/opencv.py
+++ python/opencv.py
@@ -1,17 +1,17 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("rtsp://192.168.100.10:8554/")
 
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 while True:
-    ret, frame = cap.read()             # 讀取影片的每一幀
+    ret, frame = cap.read()
     if not ret:
-        print("Cannot receive frame")   # 如果讀取錯誤，印出訊息
+        print("Cannot receive frame")
         break
-    cv2.imshow('oxxostudio', frame)     # 如果讀取成功，顯示該幀的畫面
-    if cv2.waitKey(1) == ord('q'):      # 每一毫秒更新一次，直到按下 q 結束
+    cv2.imshow('owo', frame)
+    if cv2.waitKey(1) == ord('q'):
         break
-cap.release()                           # 所有作業都完成後，釋放資源
-cv2.destroyAllWindows()                 # 結束所有視窗    
+cap.release()
+cv2.destroyAllWindows()
