ba18324033b83d446da22af92bb4cde8e11cdf20
BrianRen <46395700+RenYouchen@users.noreply.github.com>
2022-11-02 10:20:42 +0000

    a

--- CPP/owo資訊培訓/C3/problemB.cpp
+++ CPP/owo資訊培訓/C3/problemB.cpp
@@ -10,33 +10,16 @@ void s() {
         }
     }
     for (int i = 0; i < 8; i++) {
-        int count = 0;
         for (int j = 0; j < 8; j++) {
             if (in[i][j] == '#') {
-                count++;
-            }
-        }
-        if (count == 1) {
-            count = 0;
-            for (int j = 0; j < 8; j++) {
-                if (in[j][i] == '#') {
-                    count++;
-                }
-            }
-            if (count == 1) {
-                for (int j = 0; j < 8; j++) {
-                    if (in[i][j] == '#') {
-                        if (in[i - 1][j + 1] == '#' &&
-                            in[i - 1][j - 1] == '#' &&
-                            in[i + 1][j + 1] == '#' &&
-                            in[i + 1][j - 1] == '#') {
-                            cout << i << ' ' << j << '\n';
-                        }
-                    }
+                if (in[i - 1][j + 1] == '#' && in[i - 1][j - 1] == '#' &&
+                    in[i + 1][j + 1] == '#' && in[i + 1][j - 1] == '#') {
+                    cout << i+1 << ' ' << j+1 << '\n';
                 }
             }
         }
     }
+
 }
 
 int main() {
